076814f147b434c93b0a7a0e8cda8a823ed8a429
Andrzej <jedrekg2000@gmail.com>
2025-04-03 19:58:42 +0200

    Formatowanie kodu Wywołanie menu.start

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,23 @@
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        //Stworz obniekty klasy Wojownik i Łowca
-        Wojownik w1 = new Wojownik(100,100,100);
-        Łowca ł1 = new Łowca(100, 100, 0);
+        Menu.startGame();
 
-   
-   
-        //Wywołaj metody ciosLekki, ciosMocny, leczenie oraz poruszanie dla obiektów klasy Wojownik i Łowca
-        w1.ciosLekki();
-        w1.ciosMocny();
-        w1.leczenie();
-        w1.poruszanie(0);
+        // //Stworz obniekty klasy Wojownik i Łowca
+        // Wojownik w1 = new Wojownik(100,100,100);
+        // Łowca ł1 = new Łowca(100, 100, 0);
 
-        ł1.ciosLekki();
-        ł1.ciosMocny();
-        ł1.leczenie();
-        ł1.poruszanie(0);
+        // //Wywołaj metody ciosLekki, ciosMocny, leczenie oraz poruszanie dla obiektów
+        // klasy Wojownik i Łowca
+        // w1.ciosLekki();
+        // w1.ciosMocny();
+        // w1.leczenie();
+        // w1.poruszanie(0);
+
+        // ł1.ciosLekki();
+        // ł1.ciosMocny();
+        // ł1.leczenie();
+        // ł1.poruszanie(0);
+
     }
 }
