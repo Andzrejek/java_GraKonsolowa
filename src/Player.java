356cba2e9c22f66caedfe3461a934e7e66565f81
Andrzej <jedrekg2000@gmail.com>
2025-04-03 20:00:35 +0200

    Dodanie nowej klasy Player Dodanie pól klast dodanie konstruktora

---
+++ src/Player.java
@@ -0,0 +1,12 @@
+public class Player {
+    // pola klasy
+    String name;
+    int postac;
+
+    // mkonstruktor
+    public Player(String name, int postac) {
+        this.name = name;
+        this.postac = postac;
+    }
+
+}
